40d5a168232b0f6ac0e18c2712af50d0735073f5
IshaiAsor <ishaiasor@gmail.com>
2017-10-26 10:26:33 +0300

    done with part 1

--- scripts/users.js
+++ scripts/users.js
@@ -33,22 +33,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function Follow(userid){
 	var userElements = document.getElementsByClassName(userid);
-	for(var i=0;i<userElements.length;i++){
-		//find the button
-		//change the text
-		//chenge the action
-		
+	var results = users.filter(function(user) { return user.id == userid; });
+	results[0].follow = true;
+	var generalGrid = document.getElementById('general-grid');
+	var userElements = generalGrid.getElementsByClassName(userid);
 
-	}
-	users.filter(user.id = userid).follow = true;
+	var buttons = userElements[0].getElementsByTagName('input');//find the button
+	buttons[0].value = 'UnFollow';//change the text
+	buttons[0].setAttribute('onclick','UnFollow('+userid+')');//chenge the action
+    ChangeButtonColors(buttons[0],'#337ab7','#fff');
+	var followers = document.getElementById('followers').appendChild(createUserDiv(results[0],true));//get the followers grid
 
+}
 
+function UnFollow(userid){
+	var results = users.filter(function(user) {
+		return user.id == userid;
+	});
+	results[0].follow = false;
+	var generalGrid = document.getElementById('general-grid');
+	var userElements = generalGrid.getElementsByClassName(userid);
+
+	var buttons = userElements[0].getElementsByTagName('input');//find the button
+	buttons[0].value = 'Follow';//change the text
+	buttons[0].setAttribute('onclick','Follow('+userid+')');//chenge the action
+	ChangeButtonColors(buttons[0],'#fff','#337ab7');
+
+	var followers = document.getElementById('followers');//get the followers grid
+	var tmp = followers.getElementsByClassName(userid);//find the follower view object
+	followers.removeChild(tmp[0]);//remove the follower from grid
 }
 
 function createUserDiv(user,followerModel) {
     var container = document.createElement('div');
     if(!followerModel)
-	    container.className = 'col-xs-2 col-md-2';
+	    container.className = 'col-xs-2 col-md-2';//the follower model is because the bootstrap shit
     container.classList.add(user.id)  ;
 	var panel = document.createElement('div');
 	panel.className = 'panel panel-default';
@@ -62,11 +81,19 @@ function createUserDiv(user,followerModel) {
 	var button = document.createElement('input');
 	button.type = 'button';
 	button.className = 'btn btn-primary';
-	if(!user.follow)
-	button.value = 'follow';
+	if(!user.follow){
+		button.value = 'Follow';
+		button.setAttribute('onclick','Follow('+user.id+')');
+		ChangeButtonColors(button,'#fff','#337ab7');
+	}
+
 	else
-		button.value = 'unFollow';
-	button.setAttribute('click','FollowUnfollw('+user.id+')');
+	{
+		button.value = 'UnFollow';
+		button.setAttribute('onclick','UnFollow('+user.id+')');
+		ChangeButtonColors(button,'#337ab7','#fff');
+	}
+
 
 
 	var userName = document.createElement('div');
@@ -81,5 +108,10 @@ function createUserDiv(user,followerModel) {
 	container.appendChild(panel);
 return container;
 
+}
+
+function ChangeButtonColors(button,color,BColor) {
+	button.style.color = color;
+	button.style.backgroundColor = BColor;
 }
 
